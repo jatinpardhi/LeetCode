82c7a1045acd76bdfebc05760f7047a338b7ab7c
Jatin Pardhi <48383934+jatinpardhi@users.noreply.github.com>
2022-06-19 16:45:28 +0530

    Time: 142 ms (70.81%), Space: 18.1 MB (47.63%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        d1=defaultdict(lambda:0)
+        products.sort()
+        curr=''
+        for i in range(len(searchWord)):
+            curr+=searchWord[i]
+            d1[curr]=[]
+        
+        for x in products:
+            cur=''
+            for i in range(len(x)):
+                cur+=x[i]
+                if d1[cur]==0:
+                    continue
+                d1[cur].append(x)
+
+        
+        #print(d1)
+        a=[]
+        
+        currr=''
+        for i in range(len(searchWord)):
+            currr+=searchWord[i]
+            if len(d1[currr])>3:
+                a.append(d1[currr][:3])
+            else:
+                a.append(d1[currr])
+        return a
+    
