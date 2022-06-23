ff082f0162fb99170e9f7ca64acdd9f2365eb2eb
Jatin Pardhi <48383934+jatinpardhi@users.noreply.github.com>
2022-06-24 00:05:42 +0530

    Time: 68 ms (15.15%), Space: 15 MB (8.75%) - LeetHub

---
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -0,0 +1,71 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import defaultdict
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        d1=defaultdict(lambda:[])
+        def height(root,c):
+            if root==None:
+                return 0
+            d1[c].append(root.val)
+            height(root.left,c+1)
+            height(root.right,c+1)
+        c=0
+        height(root,c)
+        ans=[]
+        for x in d1.values():
+            ans.append(x)
+        return ans
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        '''
+        def height(root,h):
+            if(root==None):
+                return 0
+            hleft=height(root.left,h+1)
+            hright=height(root.right,h+1)
+           # print(max(hleft,hright)+1)
+            return(max(hleft,hright)+1)
+            
+        def lorder(root,a,count):
+            if(root==None):
+                return
+            a[count].append([root.val])
+            lorder(root.left,a,count+1)
+            lorder(root.right,a,count+1)
+            return a
+        x=0
+        h=height(root,x)
+        a=defaultdict(lambda:[])
+        ans=[]
+        
+        count=0
+        lorder(root,a,count)
+        for x in a:
+            ans.append(x)
+        return ans
+        '''
+            
+        
